85a85611cf6c84dcb3ae5a653bf90c0470b384d8
abdulrahmanabdullah <nfs056@gmail.com>
2017-10-24 01:00:14 +0300

    remove edit text , and convert layout_item_list to CardView

--- app/src/main/java/abdulrahmanjavanrd/com/prioritytaskdemo7_java/Fragments/FragB.java
+++ app/src/main/java/abdulrahmanjavanrd/com/prioritytaskdemo7_java/Fragments/FragB.java
@@ -9,11 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-
 import java.util.ArrayList;
-import java.util.List;
-
 import abdulrahmanjavanrd.com.prioritytaskdemo7_java.adapter.MyRecyclerView;
 import abdulrahmanjavanrd.com.prioritytaskdemo7_java.model.ConstantList;
 import abdulrahmanjavanrd.com.prioritytaskdemo7_java.R;
@@ -31,30 +27,20 @@ public class FragB extends Fragment{
 
     // recycler view id
     RecyclerView mRecycler ;
-    // edit text ..
-    EditText editText ;
     //onCreateView
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_b_layout,container,false);
-        editText = view.findViewById(R.id.et_add_material);
         mRecycler = view.findViewById(R.id.re_list_frag_b);
         setUpRecycler();
         return view ;
     }
 
-    public void addNewItem(View view){
-       String takeItem = editText.getText().toString();
-   List<String> localList=ConstantList.mList(takeItem);
-   // TODO: pass this list to listView or recycler view .
-
-    }
 
     public void setUpRecycler(){
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         mRecycler.setLayoutManager(layoutManager);
-        mRecycler.setHasFixedSize(true);
         ArrayList<String> list = mConstantList.getGlobalList();
         int totalItemInList = list.size();
         MyRecyclerView recyclerView = new MyRecyclerView(totalItemInList);
