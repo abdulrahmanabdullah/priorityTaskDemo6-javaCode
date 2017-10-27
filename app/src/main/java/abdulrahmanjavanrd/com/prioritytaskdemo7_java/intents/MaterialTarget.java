dd843ec70a695bf05da915584b9238f17e3fb249
abdulrahman-abdullah <nfs056@gmail.com>
2017-10-27 22:26:54 +0300

    create up button in material activity, and create new menu file called material_menu

--- app/src/main/java/abdulrahmanjavanrd/com/prioritytaskdemo7_java/intents/MaterialTarget.java
+++ app/src/main/java/abdulrahmanjavanrd/com/prioritytaskdemo7_java/intents/MaterialTarget.java
@@ -1,11 +1,16 @@
 package abdulrahmanjavanrd.com.prioritytaskdemo7_java.intents;
 
 import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.View;
+
 import abdulrahmanjavanrd.com.prioritytaskdemo7_java.R;
 import abdulrahmanjavanrd.com.prioritytaskdemo7_java.adapter.MyRecyclerView;
 import abdulrahmanjavanrd.com.prioritytaskdemo7_java.model.ConstantList;
@@ -25,13 +30,24 @@ public class MaterialTarget extends AppCompatActivity {
         materialToolbar.setTitle(R.string.material_list);
         setSupportActionBar(materialToolbar);
         recycler = findViewById(R.id.re_list_frag_b);
+
+        // enable the up button
+        ActionBar actionBar = getSupportActionBar() ;
+        actionBar.setDisplayHomeAsUpEnabled(true);
         setUpRecycler();
     }
+
 
     private void setUpRecycler(){
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recycler.setLayoutManager(layoutManager);
         myRecyclerView = new MyRecyclerView(ConstantList.getData());
         recycler.setAdapter(myRecyclerView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.material_menu,menu);
+        return true;
     }
 }
